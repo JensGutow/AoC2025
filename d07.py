2067eb1301b95f5046e8d7f0cdd3da2f93081cbf
Jens Gutow <jens_gutow@web.de>
2025-12-07 08:47:50 +0100

    day 07

--- d07.py
+++ d07.py
@@ -1,18 +1,47 @@
 from time import perf_counter as pc
+from collections import Counter as cnt
 
 def read_puzzle(file):
-    puzzle = []
-    for line in open(file).read().splitlines():
-        puzzle.append(list(map(int, line)))
-    return puzzle
+    pod = dict()
+    start = None
+    for y, line in enumerate(open(file).read().splitlines()):
+        for x, c in enumerate(line):
+            if c == 'S': start = (x,y)
+            pod[(x,y)] = c
+    return y+1, start, pod
 
 
-def solve(puzzle, N):
+def solve(puzzle):
+    (y_max, start, pods) = puzzle
+    (x,y) = start
+    beams = {x}
+    timelines = cnt([x])
     p1 = 0
-    return p1
+    for y in range(1,y_max):
+        beams2 = set()
+        timelines2 = cnt()
+        while beams:
+            x = beams.pop()
+            if pods[(x,y)] == ".": 
+                beams2.add(x)
+                timelines2[x] += timelines[x] 
+            else:
+                splitted = False
+                if (x+1,y) in pods: 
+                    beams2.add(x+1)
+                    timelines2[x+1] += timelines[x]
+                    splitted = True
+                if (x-1,y) in pods: 
+                    beams2.add(x-1)
+                    timelines2[x-1] += timelines[x]
+                    splitted = True
+                p1 += splitted
+        beams = beams2
+        timelines = timelines2 
+    return p1, sum(timelines.values())
 
 start = pc()
-puzzle = read_puzzle('d03.txt')
+puzzle = read_puzzle('d07.txt')
 print("Task 1", solve(puzzle))
 end = pc()
 print("Time", end - start)
